191f92d0e89348d3e1b7a68ef82a8176ae011451
beauwilliams <beau.williams@icloud.com>
2022-09-27 11:32:42 +1000

    init

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Cleaning up..."
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) rm -rf ./.git && rm -rf ./contracts/* && rm -rf ./test/* && rm -rf ./scripts/deploy.ts && rm -rf ./scripts/verify.ts && rm -rf ./scripts/upgrade.ts && rm -rf ./artifacts/* && rm -rf ./nyc_output/* && rm -rf ./coverage/* && rm -rf ./deployments/* && rm -rf ./cache/* && rm -rf ./open-zeppelin/* && rm -rf ./typechain/* && exit;;
+    No ) exit;;
+  esac
+done
